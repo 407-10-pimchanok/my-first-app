3a61e7f01259c6ca3156d23e8d1fb1fa91ce9822
407-10-pimchanok <54197@yupparaj.ac.th>
2026-08-28 11:27:48 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -46,7 +46,7 @@ def show_result_dialog(ans1, ans2, ans3, ans4):
 
     # ตรวจข้อ 2
     if u_ans2 == "fish":
-        st.success("✅ ข้อ 2: ถูกต้อง"
+        st.success("✅ ข้อ 2: ถูกต้อง")
         score += 1
     else:
         st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
