06af093ab64736bbff8430910b620d820815ca5c
407-10-pimchanok <54197@yupparaj.ac.th>
2026-09-11 11:37:49 +0700

    Update brainrot_game.py

--- brainrot_game.py
+++ brainrot_game.py
@@ -74,7 +74,10 @@ st.divider()
 # ----------------------------------------------------
 
 # ข้อ 1: Tung tung sahur
-st.image("https://spacebar.th/storage/tung-tung-tung-tung-sahur-meme-SPACEBAR-Hero.jpg", width=200)
+st.image(
+    "https://spacebar.th/storage/tung-tung-tung-tung-sahur-meme-SPACEBAR-Hero.jpg",
+    width=200,
+)
 ans1 = st.text_input(
     "ข้อ 1: T _ n g  t _ n g  s _ h u r 🔔",
     value=st.session_state.ans1_val,
